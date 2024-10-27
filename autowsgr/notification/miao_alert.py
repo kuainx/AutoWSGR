673171e036897bec3f357fe253db52683e8bb86f
sykounya <syokounya0828@gmail.com>
2024-10-27 15:19:44 +0800

    添加了报错跳过执行功能

--- autowsgr/notification/miao_alert.py
+++ autowsgr/notification/miao_alert.py
@@ -9,18 +9,23 @@ def load_config():
 
 
 def miao_alert(index):
-    config = load_config()
-    miao_code = config['miao_code']
-    texts = config['texts']  # 从 YAML 文件中读取 texts 列表
+    try:
+        config = load_config()
+        miao_code = config['miao_code']
+        texts = config['texts']  # 从 YAML 文件中读取 texts 列表
 
-    # 检查索引是否在列表范围内
-    if 0 <= index < len(texts):
-        text = texts[index]
-        request.urlopen(
-            'https://miaotixing.com/trigger?' + parse.urlencode({'id': miao_code, 'text': text}),
-        )
-    else:
-        print('指定的索引超出了 texts 列表的范围。')
+        # 检查索引是否在列表范围内
+        if 0 <= index < len(texts):
+            text = texts[index]
+            request.urlopen(
+                'https://miaotixing.com/trigger?'
+                + parse.urlencode({'id': miao_code, 'text': text}),
+            )
+        else:
+            print('指定的索引超出了 texts 列表的范围。')
+    except Exception as e:
+        print(f'执行 miao_alert 函数时发生错误：{e}')
+        # 可以选择记录日志或执行其他错误处理操作
 
 
 if __name__ == '__main__':
