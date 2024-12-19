7d9c9e7a129291590fec9f238f8e479674288516
X-vedeax <65024689+veadex@users.noreply.github.com>
2024-12-19 22:42:43 +0800

    feat: 支持最新的舰队问答活动 (#123)
    * 支持活动地峡之岚
    
    * 版本更新

--- examples/event.py
+++ examples/event.py
@@ -1,14 +1,14 @@
-from autowsgr.fight.event.event_2024_0930 import EventFightPlan20240930
+from autowsgr.fight.event.event_2024_1219 import EventFightPlan20241219
 from autowsgr.scripts.main import start_script
 
 
 timer = start_script('./user_settings.yaml')
 # set_support(timer,True) # 如果要在战斗前开启战役支援请取消这一行的注释
-plan = EventFightPlan20240930(
+plan = EventFightPlan20241219(
     timer,
-    plan_path='E5ADF',
+    plan_path='E2BC',
     fleet_id=4,
-)  # 修改E11CD.yaml为相对于的plan，详细的plan名可在data/plans/event/202409309查看，fleet_id为出击编队
+)  # 修改E2BC为相对于的plan，详细的plan名可在data/plans/event/202409309查看，fleet_id为出击编队
 
 
 plan.run_for_times(
