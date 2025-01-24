20fcf8453d658baf5c0c65701752f2801b1511e4
Vanillaaaa <vanillaaaajp@gmail.com>
2025-01-24 20:41:57 +0800

    feat: E1炸鱼 (#158)

--- examples/event.py
+++ examples/event.py
@@ -1,12 +1,12 @@
-from autowsgr.fight.event.event_2024_1219 import EventFightPlan20241219
+from autowsgr.fight.event.event_2025_0123 import EventFightPlan20250123
 from autowsgr.scripts.main import start_script
 
 
 timer = start_script('./user_settings.yaml')
 # set_support(timer,True) # 如果要在战斗前开启战役支援请取消这一行的注释
-plan = EventFightPlan20241219(
+plan = EventFightPlan20250123(
     timer,
-    plan_path='E2BCD',
+    plan_path='E1AB',
     fleet_id=4,
 )  # 修改E2BCD为相对于的plan，详细的plan名可在data/plans/event/20241219查看，fleet_id为出击编队
 
