9b731375662dbf07efbebbb3a6c0d2c5bbee5710
huan-yp <3051561876@qq.com>
2026-02-24 01:22:33 +0800

    fix: 决战恢复模式下的一个小 bug

--- autowsgr/ops/decisive/handlers.py
+++ autowsgr/ops/decisive/handlers.py
@@ -153,7 +153,7 @@ class DecisivePhaseHandlers(DecisiveBase):
                     selections, first_node=first_node,
                 )
 
-            if not to_buy and len(self._state.ships) == 0:
+            if not to_buy and len(self._state.ships) == 0 and self._state.is_begin():
                 _log.info("[决战] 未选择舰船, 必须购买一项 → 选择第一项")
                 self._map.buy_fleet_option(list(selections.values())[0].click_position)
                 self._map.close_fleet_overlay()
