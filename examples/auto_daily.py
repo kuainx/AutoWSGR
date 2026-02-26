42a9c2c8035180ce5aea0bd05dba1c993cede7bb
huan-yp <3051561876@qq.com>
2026-02-26 13:53:42 +0800

    清理: 删除过时的示例脚本文件

--- examples/auto_daily.py
+++
@@ -1,9 +0,0 @@
-from autowsgr.scripts.daily_api import DailyOperation
-from autowsgr.scripts.main import start_script
-
-
-timer = start_script('./user_settings.yaml')
-
-# 日常，可以实现日常出击，战役，演习等操作
-operation = DailyOperation(timer)
-operation.run()
--- examples/decisive_battle.py
+++
@@ -1,9 +0,0 @@
-from autowsgr.fight import DecisiveBattle
-from autowsgr.scripts.main import start_script
-
-
-timer = start_script('./user_settings.yaml')
-
-# 决战出击
-decisive_battle = DecisiveBattle(timer)
-decisive_battle.run_for_times(1)  # 数字为决战出击的次数
