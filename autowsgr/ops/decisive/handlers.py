3c63a6ebcfe65769c61ad6793ee51e394b69abdc
KUAI <ekuai@foxmail.com>
2026-04-20 15:21:01 +0800

    fix: shipname match (#454)
    * Update ocr.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update handlers.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- autowsgr/ops/decisive/handlers.py
+++ autowsgr/ops/decisive/handlers.py
@@ -240,7 +240,12 @@ class DecisivePhaseHandlers(DecisiveBase):
                     self._state.ships.add(name)
 
         if not self._map.close_fleet_overlay():
-            raise RuntimeError('决战关闭战备舰队界面失败')
+            _log.info('[决战] 关闭决战选船界面失败, 选择第一艘后撤退')
+            self._state.phase = DecisivePhase.RETREAT
+            _, first_value = next(iter(selections.items()))
+            self._map.buy_fleet_option(first_value.click_position)
+            if not self._map.close_fleet_overlay():
+                raise RuntimeError('关闭决战选船界面失败')
         self._state.phase = DecisivePhase.PREPARE_COMBAT
 
     def _handle_advance_choice(self) -> None:
