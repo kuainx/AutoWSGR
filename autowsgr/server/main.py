83bd6a4a216f79cf76bf0c160fc62ee90dd3b24d
0xWelt <dinghao12601@gmail.com>
2026-05-09 03:11:34 +0800

    style: fix S104 hardcoded-bind-all-interfaces
    - Add noqa comment for intentional 0.0.0.0 binding in dev server entry
    - Remove S104 from TODO ignore list

--- autowsgr/server/main.py
+++ autowsgr/server/main.py
@@ -152,4 +152,4 @@ async def ws_task(websocket: WebSocket):
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)  # noqa: S104
